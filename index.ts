a4dea00aa487c27f3e157eafb04e3dc848e382f9
luan.correia <luan.correia@intelie.com.br>
2021-09-06 16:17:48 -0300

    refactor

--- index.ts
+++ index.ts
@@ -1,15 +1,16 @@
 import dotenv from 'dotenv'
 import Register from "./src/services/utils/register";
 import Connection from "./src/services/Connection/Connection";
+import { manifest } from './manifest';
 
 if(process.env.NODE_ENV !='PRODUCTION'){
     dotenv.config()
-    console.log('running in localhost');
+    console.log('service running');
 }
 const register = new Register
 register.setStart()
 const connection = new Connection;
-const intervalInMinuts = 2;
+const intervalInMinuts = manifest.interval;
 const interval = 60000*intervalInMinuts; 
 console.log(`Started application with interval ${interval/60000} minuts`);
 
